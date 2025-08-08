ccb6fee1cb62abf9b02fd8edbf14c36fbe59cd11
zitiff <95710946+zitiff@users.noreply.github.com>
2025-08-08 14:55:22 +0300

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Counter
+{
+    private int value; 
+    public Counter(int startValue = 0)
+    {
+        value = startValue;
+    }
+    
+    public void Increment()
+    {
+        value++;
+    }
+    
+    public int GetValue()
+    {
+        return value;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Counter counter = new Counter(0); // начальное значение 0
+
+        for (int i = 0; i < 5000; i++)
+        {
+            counter.Increment();
+        }
+
+        int result = counter.GetValue();
+        Console.WriteLine($"Результат: {result}");
+
+        if (result == 5000)
+        {
+            Console.WriteLine("Тест пройден");
+        }
+        else
+        {
+            Console.WriteLine("Тест провален");
+        }
+    }
+}
